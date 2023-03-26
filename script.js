a1ad9952dc28d534e8e0491ff1c897f004d0188b
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2023-03-26 20:10:14 +0200

    added a header

---
+++ script.js
@@ -0,0 +1,17 @@
+let currentPokemon;
+
+
+async function loadPokemon() {
+    let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let response = await fetch(url);
+    let currentPokemon = await response.json();
+
+    console.log('Loaded pokemon:', currentPokemon);
+
+    renderPokemonInfo(currentPokemon);
+}
+
+
+function renderPokemonInfo(currentPokemon) {
+    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+}
